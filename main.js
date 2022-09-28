b6bd4985a359c5ec24c637f8f376941253af315a
Axel Asp <solidtop.asp@gmail.com>
2022-09-28 13:23:16 +0200

    First commit

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
-//Initialize ToDo Array
-const todoArray = [];
+const todoList = [];
 let tasksCompleted = 0;
 
+//Handle submit event
 const form = document.querySelector('form');
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     const input = document.querySelector('#input-field');
-    if (!input.value) {
-        showMessage('Please enter a task');
+    if (!input.value) { //Exit early with message if input.value is falsey/empty
+        showMessage('Please enter a task!');
         return;
     }
 
     addTodo(input.value);
     input.value = ''; //clear input field
-    clearMessage(); //Clear message
+    clearMessage(); 
 });
 
+//Handle click event from the list-item buttons
 document.querySelector('ul').addEventListener('click', e => {
-    if (e.target.id == 'button-check') {
+    if (e.target.name == 'button-check') {
         checkTodo(e);  
-    } else if (e.target.id == 'button-delete') {
+    } else if (e.target.name == 'button-delete') {
         deleteTodo(e);
     }
 });
 
-//Update TaskCompleted display
-const display = document.querySelector('#tasks-completed');
-display.innerHTML = 'Tasks Completed: ' + tasksCompleted;
-
-updateDisplay(120);
 
 function addTodo(todo) {
     const ul = document.querySelector('ul');
@@ -39,28 +35,55 @@ function addTodo(todo) {
     const iconDelete = '<i class="fa fa-trash"></i>';
     li.innerHTML = `
         <span class="todo-item">${todo}</span>
-        <button id="button-check">${iconCheck}</button>
-        <button id="button-delete">${iconDelete}</button>
+        <button name="button-check" class="button-check">${iconCheck}</button>
+        <button name="button-delete">${iconDelete}</button>
     `;
     li.classList.add('todo-list-item');
     ul.appendChild(li);
+
+    todoList.push({
+        item: li, //Store all item HTML elements
+        checked: false, //Keeping track of checking/completing
+    });
+    console.log('Todo Added');
 }
 function deleteTodo(e) {
-    const item = e.target.parentNode;
-    item.remove();
+
+    const item = e.target.parentNode; //Get <li> item from clicked button
+    const listItems = Array.from(document.querySelectorAll('li')); //Get NodeList for <li> (item) elements and convert it to Array
+    const index = listItems.indexOf(item); //Get array index of the item
+    item.remove(); //remove item element from html document
+    
+    if (todoList[index].checked) { //Update tasksCompleted if item is checked when removed
+        updateDisplay(--tasksCompleted);
+    }
+    todoList.splice(index, 1); //Then finally remove/splice index from todo array 
+    
+    console.log('Todo Deleted');
 }
 function checkTodo(e) {
+
     const item = e.target.parentNode;
-    const icon = item.querySelector('#button-check i');
-    if (item.style.textDecoration == 'line-through') {
-        item.style.textDecoration = 'none';  
-        icon.style.color = 'var(--colorButton)'; 
+
+    const listItems = Array.from(document.querySelectorAll('li'));
+    const index = listItems.indexOf(item);
+    if (todoList[index].checked) {
+
+        todoList[index].checked = false;
+        item.classList.remove('checked'); //Remove class element from html document
+        updateDisplay(--tasksCompleted);
     } else {
-        item.style.textDecoration = 'line-through';
-        icon.style.color = 'var(--colorChecked)';
+        
+        todoList[index].checked = true;
+        item.classList.add('checked'); //Add class element so we can style in CSS
+        updateDisplay(++tasksCompleted);
     }
 }
 
+function updateDisplay(tasks) {
+    const display = document.querySelector('#tasks-completed');
+    display.innerHTML = 'Tasks Completed: ' + tasks;
+}
 
 function showMessage(text) {
     const message = document.querySelector('#message');
